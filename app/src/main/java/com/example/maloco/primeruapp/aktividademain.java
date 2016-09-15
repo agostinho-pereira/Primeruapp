8f1c1b4deac5599f41594c8aaf930b88c331ba56
maloco <maloco@tosc.tl>
2016-09-15 09:34:24 -0700

    initial commit

---
+++ app/src/main/java/com/example/maloco/primeruapp/aktividademain.java
@@ -0,0 +1,13 @@
+package com.example.maloco.primeruapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aktividademain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktividademain);
+    }
+}
